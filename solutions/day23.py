69ef87e825b3ca9f94f90afaabb0266c8a39d0f2
joehall87 <joehall87@gmail.com>
2021-12-25 16:12:13 +0000

    Day 23 - done!!

--- solutions/day23.py
+++ solutions/day23.py
@@ -9,10 +9,15 @@ class Burrow:
   """
   _allowable_hallway_locs = {0, 1, 3, 5, 7, 9, 10}
 
-  def __init__(self, state, energy_used=0, prev_burrow=None):
+  def __init__(self, state, energy_used=0, room_size=2):
     self._state = state
     self._energy_used = energy_used
-    self._prev_burrow = prev_burrow
+    self._room_size = room_size
+    self._completed_state = {
+      (i, j): a
+      for i, a in enumerate("ABCD", start=1)
+      for j in range(self._room_size)
+    }
 
   @classmethod
   def from_puzzle(cls, puzzle):
@@ -22,21 +27,12 @@ class Burrow:
       for room, amphipods in enumerate(puzzle, start=1)
       for loc, amphipod in enumerate(amphipods)
     }
-    return cls(state)
+    return cls(state, room_size=len(puzzle[0]))
 
   @property
   def is_complete(self):
     """Have we arrived at the correct end-state?"""
-    return self._state == {
-      (1, 0): "A",
-      (1, 1): "A",
-      (2, 0): "B",
-      (2, 1): "B",
-      (3, 0): "C",
-      (3, 1): "C",
-      (4, 0): "D",
-      (4, 1): "D",
-    }
+    return self._state == self._completed_state
 
   @property
   def energy_used(self):
@@ -54,7 +50,11 @@ class Burrow:
     amphipod = new_state.pop(old_loc)
     new_state[new_loc] = amphipod
     extra_energy = self._calc_energy_used(amphipod, steps)
-    return Burrow(new_state, energy_used=self._energy_used + extra_energy, prev_burrow=self)
+    return Burrow(
+      new_state,
+      energy_used=self._energy_used + extra_energy,
+      room_size=self._room_size,
+    )
 
   def iter_possible_next_moves(self):
     """Iter over all possible next moves."""
@@ -63,12 +63,11 @@ class Burrow:
         yield from self._iter_possible_hallway_moves(self, amphipod, loc, can_stay_in_hall=False)
       elif not self._arrived(amphipod, room, loc):
         hall_loc = room * 2
-        if loc == 1:
-          burrow = self.new((room, loc), (0, hall_loc), steps=1)
-        elif not self.occupied(room, 1):
-          burrow = self.new((room, loc), (0, hall_loc), steps=2)
-        else:
+        blocked = any(self.occupied(room, i) for i in range(loc + 1, self._room_size))
+        if blocked:
           continue
+        steps_to_hall = self._room_size - loc
+        burrow = self.new((room, loc), (0, hall_loc), steps=steps_to_hall)
         yield from self._iter_possible_hallway_moves(burrow, amphipod, hall_loc, can_stay_in_hall=True)
 
   @classmethod
@@ -83,10 +82,9 @@ class Burrow:
           yield burrow.new((0, loc), (0, new_loc), steps)
         elif burrow.outside_correct_room_and_room_ready(amphipod, new_loc):
           side_room = new_loc // 2
-          if not burrow.occupied(side_room, 0):
-            yield burrow.new((0, loc), (side_room, 0), steps + 2)
-          else:
-            yield burrow.new((0, loc), (side_room, 1), steps + 1)
+          for i in range(burrow._room_size):
+            if not burrow.occupied(side_room, i):
+              yield burrow.new((0, loc), (side_room, i), steps=steps + burrow._room_size - i)
 
   def outside_correct_room_and_room_ready(self, amphipod, hall_loc):
     """Is this amphipod outside the correct room...and can it go in?"""
@@ -94,12 +92,11 @@ class Burrow:
     correct_room = correct_rooms[amphipod]
     if correct_room * 2 != hall_loc:
       return False
-    occupant_1 = self._state.get((correct_room, 0))
-    occupant_2 = self._state.get((correct_room, 1))
-    return (
-      (occupant_1 is None or occupant_1 == amphipod) and
-      (occupant_2 is None or occupant_2 == amphipod)
-    )
+    occupants = {
+      self._state[(correct_room, i)]
+      for i in range(self._room_size) if (correct_room, i) in self._state
+    }
+    return not occupants or occupants == {amphipod}
 
   def occupied(self, room, loc):
     """Is this location already occupied?"""
@@ -110,9 +107,12 @@ class Burrow:
     correct_room = correct_rooms[amphipod]
     if room != correct_room:
       return False
-    if loc == 1 and self._state[(room, 0)] != amphipod:
-      return False
-    return True
+    amphipods_below = {
+      self._state[(correct_room, i)]
+      for i in range(loc) if (correct_room, i) in self._state
+    }
+    arrived = not amphipods_below or amphipods_below == {amphipod}
+    return arrived
 
   @staticmethod
   def _calc_energy_used(amphimod, steps):
@@ -128,17 +128,30 @@ class Burrow:
   def __repr__(self):
     parts = []
     for room in [1, 2, 3, 4]:
-      part = f"{self._state.get((room, 0), '-')}{self._state.get((room, 1), '-')}"
+      part = "".join(self._state.get((room, i), "-") for i in range(self._room_size))
       parts.append(part)
     return ",".join(parts)
 
 
 def part_1():
   """Easy part 1."""
-  burrow = Burrow.from_puzzle(_load_start_state(test=True))
+  burrow = Burrow.from_puzzle(_load_start_state(test=False))
+  burrow = _solve(burrow)
+  print(f"\nFinal burrow: {burrow}")
+  print(f"PART 1: Least energy required to sort the amphipods is {burrow.energy_used}")
+  
+ 
+def part_2():
+  """Complex part 2."""
+  burrow = Burrow.from_puzzle(_load_start_state(test=False, part=2))
+  burrow = _solve(burrow)
+  print(f"\nFinal burrow: {burrow}")
+  print(f"PART 2: Least energy required to sort the amphipods is {burrow.energy_used}")
+
+
+def _solve(burrow):
   queue = PriorityQueue()
   queue.put(burrow)
-
   used = set()
   i = 0
   while True:
@@ -154,14 +167,7 @@ def part_1():
     for next_burrow in burrow.iter_possible_next_moves():
       if next_burrow.state_hash not in used:
         queue.put(next_burrow)
-  print(f"\nFinal burrow: {burrow}")
-  print(f"PART 1: Least energy required to sort the amphipods is {burrow.energy_used}")
-  
- 
-def part_2():
-  """Complex part 2."""
-  state = _load_start_state(test=True, part=2)
-  print(state)
+  return burrow
 
 
 def _load_start_state(test=False, part=1):
@@ -194,5 +200,5 @@ def _load_start_state(test=False, part=1):
   
 
 if __name__ == "__main__":
-  #part_1()
+  part_1()
   part_2()
