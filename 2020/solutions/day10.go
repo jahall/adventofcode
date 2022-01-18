23c0cc7facb062251aab92ec20918eeb8881a281
joehall87 <joehall87@gmail.com>
2022-01-18 09:58:21 +0000

    Day 10 - part 2

--- 2020/solutions/day10.go
+++ 2020/solutions/day10.go
@@ -14,8 +14,12 @@ type AdapterBag struct {
 	ratings []int // assumed to be sorted
 }
 
-func LoadAdapterBag() *AdapterBag {
-	file, err := os.Open("/Users/Joe/src/adventofcode/2020/data/day10.txt")
+func LoadAdapterBag(test bool) *AdapterBag {
+	suffix := ""
+	if test {
+		suffix = "_test"
+	}
+	file, err := os.Open("/Users/Joe/src/adventofcode/2020/data/day10" + suffix + ".txt")
 	check(err)
 	var ratings []int
 	scanner := bufio.NewScanner((file))
@@ -47,10 +51,62 @@ func (a *AdapterBag) CalcJoltDiffs() map[int]int {
 	return joltDiffs
 }
 
-func main() {
-	bag := LoadAdapterBag()
-	part1(bag)
-	part2(bag)
+func (a *AdapterBag) CalcNumArrangements() int {
+	numArrangements := 1
+	prevRating := 0
+	subStart := 0
+	maxRating := a.ratings[len(a.ratings)-1]
+	padded := append([]int{0}, a.ratings...)
+	padded = append(padded, maxRating+3)
+	for i, rating := range padded {
+		if rating-prevRating == 3 {
+			subRatings := padded[subStart:i]
+			arrs := a.findArrangements(subRatings)
+			// Below useful for testing
+			//fmt.Println()
+			//for _, arr := range arrs {
+			//	fmt.Println(arr)
+			//}
+			numArrangements *= len(arrs)
+			subStart = i
+		}
+		prevRating = rating
+	}
+	return numArrangements
+}
+
+func (a *AdapterBag) findArrangements(ratings []int) [][]int {
+	if len(ratings) <= 2 {
+		return [][]int{ratings}
+	}
+	arrangements := [][]int{}
+	// Drop nothing
+	for _, tail := range a.findArrangements(ratings[1:]) {
+		arr := a.concat(ratings[:1], tail)
+		arrangements = append(arrangements, arr)
+	}
+	// Drop second rating
+	if ratings[2]-ratings[0] <= 3 {
+		for _, tail := range a.findArrangements(ratings[2:]) {
+			arr := a.concat(ratings[:1], tail)
+			arrangements = append(arrangements, arr)
+		}
+	}
+	// Drop second and third ratings
+	if len(ratings) > 3 && ratings[3]-ratings[1] <= 3 {
+		for _, tail := range a.findArrangements(ratings[3:]) {
+			arr := a.concat(ratings[:2], tail)
+			arrangements = append(arrangements, arr)
+		}
+	}
+	return arrangements
+}
+
+func (a *AdapterBag) concat(arr1 []int, arr2 []int) []int {
+	// Append actually alters the array in-place! Hence copying
+	arr := make([]int, len(arr1))
+	copy(arr, arr1)
+	return append(arr, arr2...)
 }
 
 func part1(bag *AdapterBag) {
@@ -59,10 +115,19 @@ func part1(bag *AdapterBag) {
 }
 
 func part2(bag *AdapterBag) {
+	arrangements := bag.CalcNumArrangements()
+	fmt.Printf("PART 2: There are %d distinct ways of connecting\n", arrangements)
 }
 
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func main() {
+	test := len(os.Args[1:]) == 1 && os.Args[1] == "test"
+	bag := LoadAdapterBag(test)
+	part1(bag)
+	part2(bag)
 }
