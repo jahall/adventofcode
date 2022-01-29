d5283f90ff2fd339591df792b4c1679612b8119a
joehall87 <joehall87@gmail.com>
2022-01-29 14:10:23 +0000

    Day 19 - part 2

--- 2020/solutions/day19.go
+++ 2020/solutions/day19.go
@@ -83,7 +83,8 @@ func (r *Rules) Update(line string) {
 	r.rules[id] = rule
 }
 
-func (r *Rules) ValidMessage(ruleId int, message string) bool {
+func (r *Rules) ValidMessage(message string) bool {
+	ruleId := 0
 	matches, exists := r.ruleMatches[ruleId]
 	if !exists {
 		// Cache previous results cos its slooooooow
@@ -96,6 +97,49 @@ func (r *Rules) ValidMessage(ruleId int, message string) bool {
 		}
 	}
 	return false
+}
+
+// A valid message is a series of valid "42" strings followed by
+// a shorter series of "31" messages
+func (r *Rules) ValidMessageFixed(message string) bool {
+	matches42, exists := r.ruleMatches[42]
+	matches31, _ := r.ruleMatches[31]
+	if !exists {
+		matches42 = r.rules[42].Matches(r.rules)
+		matches31 = r.rules[31].Matches(r.rules)
+		r.ruleMatches[42] = matches42
+		r.ruleMatches[31] = matches31
+	}
+	num42s, offset := r.findSubMatches(message, matches42, 0)
+	num31s, offset := r.findSubMatches(message, matches31, offset)
+	return num42s > 0 && num31s > 0 && num42s > num31s && offset == len(message)
+}
+
+func (r *Rules) findSubMatches(message string, matches []string, offset int) (int, int) {
+	numMatches := 0
+	for {
+		foundMatch := false
+		for _, match := range matches {
+			end := min(offset+len(match), len(message))
+			if message[offset:end] == match {
+				numMatches++
+				offset += len(match)
+				foundMatch = true
+				break
+			}
+		}
+		if !foundMatch {
+			break
+		}
+	}
+	return numMatches, offset
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func LoadRulesAndMessages(test bool) (*Rules, []string) {
@@ -127,15 +171,26 @@ func LoadRulesAndMessages(test bool) (*Rules, []string) {
 func part1(rules *Rules, messages []string) {
 	count := 0
 	for _, m := range messages {
-		if rules.ValidMessage(0, m) {
+		if rules.ValidMessage(m) {
 			count++
 		}
 	}
 	fmt.Printf("PART 1: %d valid messages\n", count)
 }
 
+func part2(rules *Rules, messages []string) {
+	count := 0
+	for _, m := range messages {
+		if rules.ValidMessageFixed(m) {
+			count++
+		}
+	}
+	fmt.Printf("PART 2: %d valid messages based on fixed rules\n", count)
+}
+
 func main() {
 	test := len(os.Args[1:]) == 1 && os.Args[1] == "test"
 	rules, messages := LoadRulesAndMessages(test)
 	part1(rules, messages)
+	part2(rules, messages)
 }
