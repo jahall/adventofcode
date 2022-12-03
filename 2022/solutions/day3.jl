50a66e30f7483b308ff586b445440032f5fe6e5a
Joseph Hall <joseph.hall@bp.com>
2022-12-03 09:00:49 +0000

    Day 3 part 2

--- 2022/solutions/day3.jl
+++ 2022/solutions/day3.jl
@@ -13,16 +13,12 @@ function split(rucksack)
     (compartment1, compartment2)
 end
 
-priorities = Dict(
-    'a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5, 'f' => 6, 'g' => 7,
-    'h' => 8, 'i' => 9, 'j' => 10, 'k' => 11, 'l' => 12, 'm' => 13, 'n' => 14,
-    'o' => 15, 'p' => 16, 'q' => 17, 'r' => 18, 's' => 19, 't' => 20, 'u' => 21,
-    'v' => 22, 'w' => 23, 'x' => 24, 'y' => 25, 'z' => 26,
-    'A' => 27, 'B' => 28, 'C' => 29, 'D' => 30, 'E' => 31, 'F' => 32, 'G' => 33,
-    'H' => 34, 'I' => 35, 'J' => 36, 'K' => 37, 'L' => 38, 'M' => 39, 'N' => 40,
-    'O' => 41, 'P' => 42, 'Q' => 43, 'R' => 44, 'S' => 45, 'T' => 46, 'U' => 47,
-    'V' => 48, 'W' => 49, 'X' => 50, 'Y' => 51, 'Z' => 52,
-)
+priorities = Dict()
+items = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for (priority, item) in enumerate(items)
+    global priorities
+    priorities[item] = priority
+end
 
 # Part 1
 rucksacks = get_rucksacks()
@@ -34,3 +30,14 @@ for rucksack in rucksacks
     score += priorities[i]
 end
 println("PART 1: $score")
+
+# Part 2
+score = 0
+n = length(rucksacks)
+for i = 1:3:n-2
+    global score
+    (r1, r2, r3) = view(rucksacks, i:i + 2)
+    badge = first(intersect(r1, intersect(r2, r3)))
+    score += priorities[badge]
+end
+println("PART 2: $score")
