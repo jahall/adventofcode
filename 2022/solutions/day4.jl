792c53fa405160ff2c869c6317773d896ab9fe1b
joehall87 <joehall87@gmail.com>
2022-12-04 05:30:41 +0000

    Progress

--- 2022/solutions/day4.jl
+++ 2022/solutions/day4.jl
@@ -1,17 +1,29 @@
-"Utility function to load vector of stuff"
-function get_stuff()
+"Utility function to load vector of pairs"
+function get_range_pairs()
     root = dirname(dirname(@__FILE__))
     path = joinpath(root, "data", "day4.txt")
     open(path) do file
-        return readlines(file)
+        lines = readlines(file)
+        pairs = [split(line, ",") for line in lines]
+        return [
+            (
+                (parse(Int64, split(p1, "-")[1]), parse(Int64, split(p1, "-")[2])),
+                (parse(Int64, split(p2, "-")[1]), parse(Int64, split(p2, "-")[2]))
+            )
+            for (p1, p2) in pairs
+        ]
     end
 end
 
 # Part 1
-stuff = get_stuff()
+pairs = get_range_pairs()
 score = 0
+for (p1_s, p1_e), (p2_s, p2_e) in pairs
+    global score
+    
+end
 println("PART 1: $score")
 
 # Part 2
-score = 0s
+score = 0
 println("PART 2: $score")
