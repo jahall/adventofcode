f88b786ef34ef6f7fe81935951152abaf3a615a0
joehall87 <joehall87@gmail.com>
2022-12-06 06:13:30 +0000

    Day6

--- 2022/solutions/day6.jl
+++ 2022/solutions/day6.jl
@@ -1,21 +1,36 @@
-
 "Utility function to load stuff"
-function get_stuff()
+function get_datastream()
     root = dirname(dirname(@__FILE__))
     path = joinpath(root, "data", "day6.txt")
     open(path) do file
-        return readlines(file)
+        return collect(readlines(file)[1])
+    end
+end
+
+"Common functionality"
+function find_marker(stream, ndistinct)
+    buffer = copy(view(stream, 1:ndistinct - 1))
+    for i = ndistinct:length(stream)
+        append!(buffer, stream[i])
+        if length(Set(buffer)) == ndistinct
+            return i
+        end
+        popfirst!(buffer)
     end
 end
 
 "Part 1"
 function part1()
-    println("PART 1:")
+    datastream = get_datastream()
+    index = find_marker(datastream, 4)
+    println("PART 1: $index")
 end
 
 "Part 2"
 function part2()
-    println("PART 2:")
+    datastream = get_datastream()
+    index = find_marker(datastream, 14)
+    println("PART 2: $index")
 end
 
 part1()
