534bad863d448a63444fef3de9d1f2eb8c5153b1
Joseph Hall <joseph.hall@bp.com>
2022-12-09 10:46:56 +0000

    Placeholders

---
+++ 2022/solutions/day12.jl
@@ -0,0 +1,21 @@
+"Utility function to load the stuff"
+function get_stuff()
+    root = dirname(dirname(@__FILE__))
+    path = joinpath(root, "data", "day12.txt")
+    open(path) do file
+        return readlines(file)
+    end
+end
+
+"Part 1"
+function part1()
+    println("PART 1:")
+end
+
+"Part 2"
+function part2()
+    println("PART 2:")
+end
+
+part1()
+part2()
