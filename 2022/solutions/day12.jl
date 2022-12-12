000b487f0b5b534ac9be8b894c83118d9ea37bd8
Joseph Hall <joseph.hall@bp.com>
2022-12-12 08:59:33 +0000

    Day 12 part 1

--- 2022/solutions/day12.jl
+++ 2022/solutions/day12.jl
@@ -1,15 +1,62 @@
+using DataStructures
+
 "Utility function to load the stuff"
-function get_stuff()
+function get_grid()
     root = dirname(dirname(@__FILE__))
     path = joinpath(root, "data", "day12.txt")
+    to_num(char) = Int64(codepoint(char)) - 96
+    grid::Vector{Vector{Int64}} = []
+    start, target = (-1, -1), (-1, -1)
     open(path) do file
-        return readlines(file)
+        for (i, line) in enumerate(readlines(file))
+            push!(grid, [])
+            for (j, char) in enumerate(line)
+                if char == 'S'
+                    start = (i, j)
+                    val = 1
+                elseif char == 'E'
+                    target = (i, j)
+                    val = 26
+                else
+                    val = to_num(char)
+                end
+                push!(grid[end], val)
+            end
+        end
     end
+    grid, start, target
+end
+
+"Get neighbors"
+function neighbors(grid, point)
+    r, c = point
+    rows, cols = length(grid), length(grid[1])
+    neighbors = [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]
+    is_valid(p) = (p[1] >= 1) && (p[1] <= rows) && (p[2] >= 1) && (p[2] <= cols)
+    neighbors = filter(is_valid, neighbors)
+    return [((r, c), grid[r][c]) for (r, c) in neighbors]
 end
 
 "Part 1"
 function part1()
-    println("PART 1:")
+    grid, start, target = get_grid()
+    pq = PriorityQueue()
+    visited = Set([start])
+    enqueue!(pq, (start, 1, 0), 0)
+    path = -1
+    while true
+        (node, h, path) = dequeue!(pq)
+        if node == target
+            break
+        end
+        for (neighbor, n_h) in neighbors(grid, node)
+            if n_h <= h + 1 && !in(neighbor, visited)
+                push!(visited, neighbor)
+                enqueue!(pq, (neighbor, n_h, path + 1), path + 1)
+            end
+        end
+    end
+    println("PART 1: $path")
 end
 
 "Part 2"
