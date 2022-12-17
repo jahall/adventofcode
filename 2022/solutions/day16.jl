baad9b9a8c78f7f94058d90452744fd532629c3d
joehall87 <joehall87@gmail.com>
2022-12-17 22:08:48 +0000

    Day 17 ...after an hour

--- 2022/solutions/day16.jl
+++ 2022/solutions/day16.jl
@@ -1,64 +1,29 @@
 using Combinatorics
-using DataStructures
-
-"Find shortest path - Djikastra"
-function find_shortest_path(neighbours, start, target)
-    pq = PriorityQueue()
-    visited = Set([start])
-    enqueue!(pq, (start, 0), 0)
-    while length(pq) > 0
-        (valve, path_length) = dequeue!(pq)
-        if valve == target
-            return path_length
-        end
-        for neighbour in neighbours[valve]
-            if !in(neighbour, visited)
-                push!(visited, neighbour)
-                enqueue!(pq, (neighbour, path_length + 1), path_length + 1)
-            end
-        end
-    end
-    shortest
-end
 
 "Handy holder for network values"
 struct Network
     flow_rates::Dict{String, Int64}
     neighbours::Dict{String, Vector{String}}
-    useful_valves::Vector{String}
-    shortest_paths::Dict{Tuple{String, String}, Int64}
+    valves::Vector{String}
+    useful::Vector{String}
 
-    function Network(flow_rates, neighbours)
-        useful_valves = sort([name for (name, rate) in flow_rates if rate > 0])
-        shortest_paths = Dict([
-            (v1, v2) => find_shortest_path(neighbours, v1, v2)
-            for (v1, v2) in combinations(["AA", useful_valves...], 2)
-        ])
-        new(flow_rates, neighbours, useful_valves, shortest_paths)
-    end
+    Network(flow_rates, neighbours) = new(
+        flow_rates,
+        neighbours,
+        sort([name for (name, _) in flow_rates]),
+        sort([name for (name, rate) in flow_rates if rate > 0]),
+    )
 end
 
-"Get the shortest path between two valves"
-function shortest_path(network::Network, v1::String, v2::String)
-    paths = network.shortest_paths
-    haskey(paths, (v1, v2)) ? paths[(v1, v2)] : paths[(v2, v1)]
-end
+"Current state of the network"
+struct State
+    turned_on::Any
+    my_location::String
+    elephant_location::String
 
-"Calc total pressure for a given order of visiting valves"
-function calc_total_pressure(network::Network, visting_order::Vector{String}, time::Int64)
-    prev_valve = "AA"
-    pressure = 0
-    elapsed = 0
-    for valve in visting_order
-        elapsed += shortest_path(network, prev_valve, valve) + 1
-        remaining = time - elapsed
-        if remaining <= 0
-            break
-        end
-        pressure += network.flow_rates[valve] * remaining
-        prev_valve = valve
-    end
-    pressure
+    # assume the elephant stays put
+    State(turned_on, my_loc) = new(turned_on, my_loc, "AA")
+    State(turned_on, my_loc, elephant_loc) = new(turned_on, my_loc, elephant_loc)
 end
 
 "Utility function to load the network"
@@ -80,26 +45,132 @@ function get_network()
     Network(flow_rates, neighbours)
 end
 
+"Initialize the value function for me only"
+function init_values_me_only(network::Network)
+    values = Dict{State, Int64}(
+        State(Tuple(subset), valve) => 0
+        for valve in network.valves
+        for subset in combinations(network.useful)
+    )
+    for valve in network.valves
+        values[State((), valve)] = 0
+    end
+    values
+end
+
+"Initialize the value function including the elephant"
+function init_values_with_elephant(network::Network)
+    values = Dict{State, Int64}(
+        State(Tuple(subset), my_valve, elephant_valve) => 0
+        for my_valve in network.valves
+        for elephant_valve in network.valves
+        for subset in combinations(network.useful)
+    )
+    for my_valve in network.valves
+        for elephant_valve in network.valves
+            values[State((), my_valve, elephant_valve)] = 0
+        end
+    end
+    values
+end
+
+"Pre-calculate and store pressure increases"
+function calc_pressure_increases(network::Network)
+    increases = Dict{Any, Int64}(() => 0)
+    for subset in combinations(network.useful)
+        increases[Tuple(subset)] = sum(network.flow_rates[valve] for valve in subset)
+    end
+    increases
+end
+
 "Part 1"
 function part1()
     network = get_network()
-    best = 0
-    for combo in permutations(network.useful_valves, length(network.useful_valves))
-        pressure = calc_total_pressure(network, combo, 30)
-        if pressure > best
-            best = pressure
+    values = init_values_me_only(network)
+    increases = calc_pressure_increases(network)
+    for iteration = 1:30
+        println(iteration)
+        next_values = copy(values)
+        for (state, _) in values
+            max_value = values[state]
+            # try turning this one on, if it has a flow
+            if !in(state.my_location, state.turned_on) && in(state.my_location, network.useful)
+                turned_on = Tuple(sort([state.turned_on..., state.my_location]))
+                max_value = max(max_value, values[State(turned_on, state.my_location)])
+            end
+            # try moving to a neighbour
+            for next in network.neighbours[state.my_location]
+                max_value = max(max_value, values[State(state.turned_on, next)])
+            end
+            # apply the current pressure increase
+            increase = increases[state.turned_on]
+            # update the values
+            next_values[state] = max_value + increase
         end
+        values = next_values
     end
-    println("PART 1: $best")
+    val = values[State((), "AA")]
+    println("PART 1: $val")
 end
 
 "Part 2"
 function part2()
     network = get_network()
-    # do same as part one but split each permutation between me and elephant
-    println("PART 2:")
+    values = init_values_with_elephant(network)
+    increases = calc_pressure_increases(network)
+    for iteration = 1:26
+        println(iteration)
+        next_values = copy(values)
+        for (state, _) in values
+            max_value = values[state]
+            # try both of us turning one on (if we're in separate useful places)
+            if (
+                state.my_location != state.elephant_location &&
+                !in(state.my_location, state.turned_on) &&
+                in(state.my_location, network.useful) &&
+                !in(state.elephant_location, state.turned_on) &&
+                in(state.elephant_location, network.useful)
+            )
+                turned_on = Tuple(sort([state.turned_on..., state.my_location, state.elephant_location]))
+                max_value = max(max_value, values[State(turned_on, state.my_location, state.elephant_location)])
+            end
+            # try just me turning one on and the elephant moving
+            if (
+                !in(state.my_location, state.turned_on) &&
+                in(state.my_location, network.useful)
+            )
+                turned_on = Tuple(sort([state.turned_on..., state.my_location]))
+                for next in [network.neighbours[state.elephant_location]..., state.elephant_location]
+                    max_value = max(max_value, values[State(turned_on, state.my_location, next)])
+                end
+            end
+            # try the elephant turning one on and me moving
+            if (
+                !in(state.elephant_location, state.turned_on) &&
+                in(state.elephant_location, network.useful)
+            )
+                turned_on = Tuple(sort([state.turned_on..., state.elephant_location]))
+                for next in [network.neighbours[state.my_location]..., state.my_location]
+                    max_value = max(max_value, values[State(turned_on, next, state.elephant_location)])
+                end
+            end
+            # try both of us moving
+            for me_next in [network.neighbours[state.my_location]..., state.my_location]
+                for elephant_next in [network.neighbours[state.elephant_location]..., state.elephant_location]
+                    max_value = max(max_value, values[State(state.turned_on, me_next, elephant_next)])
+                end
+            end
+            # apply the current pressure increase
+            increase = increases[state.turned_on]
+            # update the values
+            next_values[state] = max_value + increase
+        end
+        values = next_values
+    end
+    val = values[State((), "AA", "AA")]
+    println("PART 2: $val")
 end
 
-# 1 hour to code (after some inspiration) ...but still too slow even for part 1!
+# 3 hours to code
 part1()
-part2()
+part2()  # ...around 18 hours to run!
--- 2022/solutions/day17.jl
+++ 2022/solutions/day17.jl
@@ -1,16 +1,161 @@
+"Struct for a point"
+struct Point
+    x::Int64
+    y::Int64
+end
 
-"Utility function to load the stuff"
-function get_cliff()
-    root = dirname(dirname(@__FILE__))
-    path = joinpath(root, "data", "day17_test.txt")
-    open(path) do file
-        return readlines(file)
+"Struct for a rock structure."
+struct Rock
+    points::Set{Point}
+    xmin::Int64
+    ymin::Int64
+    xmax::Int64
+    ymax::Int64
+
+    Rock(points::Set{Point}) = new(
+        points,
+        min([p.x for p in points]...),
+        min([p.y for p in points]...),
+        max([p.x for p in points]...),
+        max([p.y for p in points]...),
+    )
+
+    function Rock(shape::Char, y_offset::Int64)
+        if shape == '-'
+            points = [
+                Point(x + 2, y_offset)
+                for x in [1,2,3,4]
+            ]
+        elseif shape == '+'
+            points = [
+                Point(x + 2, y + y_offset)
+                for (x, y) in [(1,2), (2,1), (2,2), (2,3), (3,2)]
+            ]
+        elseif shape == '⅃'
+            points = [
+                Point(x + 2, y + y_offset)
+                for (x, y) in [(1,1), (2,1), (3,1), (3,2), (3,3)]
+            ]
+        elseif shape == '|'
+            points = [
+                Point(2, y + y_offset)
+                for y in [1,2,3,4]
+            ]
+        elseif shape == '□'
+            points = [
+                Point(x + 2, y + y_offset)
+                for x in [1,2]
+                for y in [1,2]
+            ]
+        end
+        new(Set(points))
     end
 end
 
+"Tower is a stack of rocks"
+mutable struct Tower
+    rocks::Vector{Rock}
+    height::Int64
+
+    Tower() = new(Vector{Rock}[], 0)
+end
+
+"Add a rock to the tower, and update its height"
+function add(rock::Rock, tower::Tower)
+    push!(tower.rocks, rock)
+    if rock.ymax > tower.height
+        tower.height = rock.ymax
+    end
+end
+
+"Does a rock overlap with another rock"
+function overlaps(rock1::Rock, rock2::Rock)
+    length(intersect(rock1.points, rock2.points)) > 0
+end
+
+"Does a rock overlap with the tower"
+function overlaps(rock::Rock, tower::Tower)
+    for tower_rock in tower.rocks
+        if overlaps(rock, tower_rock)
+            return true
+        end
+    end
+    false
+end
+
+"Move the rock"
+function move(rock::Rock, direction::Char, left_wall::Int64, right_wall::Int64)
+    if (
+        (direction == '<' && rock.xmin > left_wall + 1) ||
+        (direction == '>' && rock.xmax < right_wall - 1) ||
+        (direction == 'v')
+    )
+        rock = Rock(Set(move(point, direction) for point in rock.points))
+    end
+    rock
+end
+
+"Move the point"
+function move(point::Point, direction::Char)
+    if direction == '<'
+        point = Point(point.x - 1, point.y)
+    elseif direction == '>'
+        point = Point(point.x + 1, point.y)
+    elseif direction == 'v'
+        point = Point(point.x, point.y - 1)
+    end
+    point
+end
+
+"Utility function to load the movements"
+function get_movements()
+    root = dirname(dirname(@__FILE__))
+    path = joinpath(root, "data", "day17_test.txt")
+    open(path) do file
+        return readlines(file)[1]
+    end
+end
+
+"Get next rock"
+function next_rock(num::Int64, tower_height::Int64)
+    shape = ['-', '+', '⅃', '|', '□'][mod(num - 1, 5) + 1]
+    Rock(shape, tower_height + 4)
+end
+
+"Get next movement"
+function next(movements::String, index::Int64)
+    next_index = index < length(movements) ? index + 1 : 1
+    next_index, movements[next_index]
+end
+
+
 "Part 1"
 function part1()
-    println("PART 1:")
+    tower = Tower()
+    movements = get_movements()
+    movement_index = 1
+    for num = 1:2022
+        println(num)
+        rock = next_rock(num, tower.height)
+        while true
+            # move left or right (if possible)
+            movement_index, movement = next(movements, movement_index)
+            next_rock = move(rock, movement, 0, 8)
+            if !overlaps(rock, tower)
+                rock = next_rock
+            end
+            # move down (if possible)
+            next_rock = move(rock, 'v', 0, 8)
+            if !overlaps(rock, tower) && rock.ymin > 0
+                rock = next_rock
+            else
+                add(rock, tower)
+                break
+            end
+        end
+    end
+    height = tower.height
+    println("PART 1: $height")
 end
 
 "Part 2"
@@ -18,6 +163,6 @@ function part2()
     println("PART 2:")
 end
 
-# x hours
+# 1 hour so far...
 part1()
 part2()
