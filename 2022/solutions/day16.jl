9bf3b55722ed7b7f9d5258ac11ee1f74439318ab
joehall87 <joehall87@gmail.com>
2022-12-17 09:16:43 +0000

    Day 16...more attempts

--- 2022/solutions/day16.jl
+++ 2022/solutions/day16.jl
@@ -1,42 +1,70 @@
+using Combinatorics
 using DataStructures
+
+"Find shortest path - Djikastra"
+function find_shortest_path(neighbours, start, target)
+    pq = PriorityQueue()
+    visited = Set([start])
+    enqueue!(pq, (start, 0), 0)
+    while length(pq) > 0
+        (valve, path_length) = dequeue!(pq)
+        if valve == target
+            return path_length
+        end
+        for neighbour in neighbours[valve]
+            if !in(neighbour, visited)
+                push!(visited, neighbour)
+                enqueue!(pq, (neighbour, path_length + 1), path_length + 1)
+            end
+        end
+    end
+    shortest
+end
 
 "Handy holder for network values"
 struct Network
     flow_rates::Dict{String, Int64}
     neighbours::Dict{String, Vector{String}}
-    useful_valves::Set{String}
+    useful_valves::Vector{String}
+    shortest_paths::Dict{Tuple{String, String}, Int64}
 
-    Network(flow_rates, neighbours) = new(
-        flow_rates,
-        neighbours,
-        Set([name for (name, rate) in flow_rates if rate > 0]),
-    )
+    function Network(flow_rates, neighbours)
+        useful_valves = sort([name for (name, rate) in flow_rates if rate > 0])
+        shortest_paths = Dict([
+            (v1, v2) => find_shortest_path(neighbours, v1, v2)
+            for (v1, v2) in combinations(["AA", useful_valves...], 2)
+        ])
+        new(flow_rates, neighbours, useful_valves, shortest_paths)
+    end
 end
 
-"Current state of the network"
-struct State
-    minute::Int64
-    pressure::Int64
-    incremental::Int64
-    history::String
-    turned_on::Set{String}
-    my_location::String
-    elephant_location::String
+"Get the shortest path between two valves"
+function shortest_path(network::Network, v1::String, v2::String)
+    paths = network.shortest_paths
+    haskey(paths, (v1, v2)) ? paths[(v1, v2)] : paths[(v2, v1)]
+end
 
-    State() = new(0, 0, 0, "", Set{String}(), "AA", "AA")
-    # assume the elephant stays put
-    State(minute, pressure, incremental, history, turned_on, my_loc) = new(
-        minute, pressure, incremental, history, turned_on, my_loc, "AA"
-    )
-    State(minute, pressure, incremental, history, turned_on, my_loc, elephant_loc) = new(
-        minute, pressure, incremental, history, turned_on, my_loc, elephant_loc
-    )
+"Calc total pressure for a given order of visiting valves"
+function calc_total_pressure(network::Network, visting_order::Vector{String}, time::Int64)
+    prev_valve = "AA"
+    pressure = 0
+    elapsed = 0
+    for valve in visting_order
+        elapsed += shortest_path(network, prev_valve, valve) + 1
+        remaining = time - elapsed
+        if remaining <= 0
+            break
+        end
+        pressure += network.flow_rates[valve] * remaining
+        prev_valve = valve
+    end
+    pressure
 end
 
 "Utility function to load the network"
 function get_network()
     root = dirname(dirname(@__FILE__))
-    path = joinpath(root, "data", "day16_test.txt")
+    path = joinpath(root, "data", "day16.txt")
     flow_rates::Dict{String, Int64} = Dict()
     neighbours::Dict{String, Vector{String}} = Dict()
     open(path) do file
@@ -55,54 +83,11 @@ end
 "Part 1"
 function part1()
     network = get_network()
-
-    pq = PriorityQueue(Base.Order.Reverse)
-    enqueue!(pq, State(), 0)
     best = 0
-    while length(pq) > 0
-        state = dequeue!(pq)
-        # we've arrived!
-        if state.minute == 31
-            if state.pressure > best
-                best = state.pressure
-            end
-            continue
-        end
-        # common next params
-        next_minute = state.minute + 1
-        next_pressure = state.pressure + state.incremental
-        history = state.history * "/" * state.my_location
-        # nothing more we can do now that everything is on...
-        if length(state.turned_on) == length(network.useful_valves)
-            end_pressure = state.pressure + state.incremental * (31 - state.minute)
-            if end_pressure > best
-                best = state.pressure
-            end
-            continue
-        end
-        # try turning this one on, if it is useful
-        if !in(state.my_location, state.turned_on) && in(state.my_location, network.useful_valves)
-            next = State(
-                next_minute,
-                next_pressure + network.flow_rates[state.my_location],
-                state.incremental + network.flow_rates[state.my_location],
-                history,
-                union(state.turned_on, Set([state.my_location])),
-                state.my_location,
-            )
-            enqueue!(pq, next, next.pressure)
-        end
-        # try moving to a neighbour
-        for nbr in network.neighbours[state.my_location]
-            next = State(
-                next_minute,
-                next_pressure,
-                state.incremental,
-                history,
-                state.turned_on,
-                nbr,
-            )
-            enqueue!(pq, next, next.pressure)
+    for combo in permutations(network.useful_valves)
+        pressure = calc_total_pressure(network, combo, 30)
+        if pressure > best
+            best = pressure
         end
     end
     println("PART 1: $best")
@@ -116,4 +101,4 @@ end
 
 # 3 hours
 part1()
-part2()
+#part2()
