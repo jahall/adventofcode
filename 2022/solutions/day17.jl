4d5225ea28fc496257dc1da586efc1cb74429a57
joehall87 <joehall87@gmail.com>
2022-12-18 10:25:22 +0000

    Day 17 - part 1

--- 2022/solutions/day17.jl
+++ 2022/solutions/day17.jl
@@ -12,18 +12,21 @@ struct Rock
     xmax::Int64
     ymax::Int64
 
-    Rock(points::Set{Point}) = new(
-        points,
-        min([p.x for p in points]...),
-        min([p.y for p in points]...),
-        max([p.x for p in points]...),
-        max([p.y for p in points]...),
-    )
+    function Rock(points::Set{Point})
+        #println([p.x for p in points], [p.y for p in points])
+        new(
+            points,
+            min([p.x for p in points]...),
+            min([p.y for p in points]...),
+            max([p.x for p in points]...),
+            max([p.y for p in points]...),
+        )
+    end
 
     function Rock(shape::Char, y_offset::Int64)
         if shape == '-'
             points = [
-                Point(x + 2, y_offset)
+                Point(x + 2, y_offset + 1)
                 for x in [1,2,3,4]
             ]
         elseif shape == '+'
@@ -38,7 +41,7 @@ struct Rock
             ]
         elseif shape == '|'
             points = [
-                Point(2, y + y_offset)
+                Point(3, y + y_offset)
                 for y in [1,2,3,4]
             ]
         elseif shape == '□'
@@ -48,21 +51,22 @@ struct Rock
                 for y in [1,2]
             ]
         end
-        new(Set(points))
+        # can't do `new` or we end up with random xmin, xmax, etc!!
+        Rock(Set(points))
     end
 end
 
 "Tower is a stack of rocks"
 mutable struct Tower
-    rocks::Vector{Rock}
+    points::Set{Point}
     height::Int64
 
-    Tower() = new(Vector{Rock}[], 0)
+    Tower() = new(Set{Point}(), 0)
 end
 
 "Add a rock to the tower, and update its height"
 function add(rock::Rock, tower::Tower)
-    push!(tower.rocks, rock)
+    union!(tower.points, rock.points)
     if rock.ymax > tower.height
         tower.height = rock.ymax
     end
@@ -73,14 +77,9 @@ function overlaps(rock1::Rock, rock2::Rock)
     length(intersect(rock1.points, rock2.points)) > 0
 end
 
-"Does a rock overlap with the tower"
+"Does a rock overlap with a tower"
 function overlaps(rock::Rock, tower::Tower)
-    for tower_rock in tower.rocks
-        if overlaps(rock, tower_rock)
-            return true
-        end
-    end
-    false
+    length(intersect(rock.points, tower.points)) > 0
 end
 
 "Move the rock"
@@ -110,7 +109,7 @@ end
 "Utility function to load the movements"
 function get_movements()
     root = dirname(dirname(@__FILE__))
-    path = joinpath(root, "data", "day17_test.txt")
+    path = joinpath(root, "data", "day17.txt")
     open(path) do file
         return readlines(file)[1]
     end
@@ -119,7 +118,7 @@ end
 "Get next rock"
 function next_rock(num::Int64, tower_height::Int64)
     shape = ['-', '+', '⅃', '|', '□'][mod(num - 1, 5) + 1]
-    Rock(shape, tower_height + 4)
+    Rock(shape, tower_height + 3)
 end
 
 "Get next movement"
@@ -128,25 +127,23 @@ function next(movements::String, index::Int64)
     next_index, movements[next_index]
 end
 
-
-"Part 1"
-function part1()
+"Build a tower for a certain number of iterations"
+function build_tower(iterations::Int64)
     tower = Tower()
     movements = get_movements()
-    movement_index = 1
-    for num = 1:2022
-        println(num)
+    movement_index = 0
+    for num = 1:iterations
         rock = next_rock(num, tower.height)
         while true
             # move left or right (if possible)
             movement_index, movement = next(movements, movement_index)
             next_rock = move(rock, movement, 0, 8)
-            if !overlaps(rock, tower)
+            if !overlaps(next_rock, tower)
                 rock = next_rock
             end
             # move down (if possible)
             next_rock = move(rock, 'v', 0, 8)
-            if !overlaps(rock, tower) && rock.ymin > 0
+            if !overlaps(next_rock, tower) && next_rock.ymin > 0
                 rock = next_rock
             else
                 add(rock, tower)
@@ -154,15 +151,38 @@ function part1()
             end
         end
     end
+    tower
+end
+
+"Nice utility to show current status"
+function show(tower::Tower, rock::Rock)
+    println()
+    for y in max(tower.height, rock.ymax) + 1:-1:1
+        print("|")
+        for x in 1:7
+            p = Point(x, y)
+            print(in(p, rock.points) ? "@" : in(p, tower.points) ? "#" : ".")
+        end
+        println("|")
+    end
+    println("+-------+")
+    println()
+end
+
+"Part 1"
+function part1()
+    tower = build_tower(2022)
     height = tower.height
     println("PART 1: $height")
 end
 
 "Part 2"
 function part2()
-    println("PART 2:")
+    tower = build_tower(1_000_000_000_000)
+    height = tower.height
+    println("PART 2: $height")
 end
 
-# 1 hour so far...
+# 2 hours for part 1
 part1()
-part2()
+#part2()
