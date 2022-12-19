590babe93a4b9bf5bb22528a630bcd0e11b58a93
joehall87 <joehall87@gmail.com>
2022-12-19 10:58:08 +0000

    Placeholders

---
+++ 2022/solutions/day23.jl
@@ -0,0 +1,22 @@
+"Utility function to load the stuff"
+function get_droplets()
+    root = dirname(dirname(@__FILE__))
+    path = joinpath(root, "data", "day23_test.txt")
+    open(path) do file
+        return readlines(file)
+    end
+end
+
+"Part 1"
+function part1()
+    println("PART 1:")
+end
+
+"Part 2"
+function part2()
+    println("PART 2:")
+end
+
+# x mins
+part1()
+part2()
