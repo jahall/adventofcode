8b196fc279b256bd4a895138b276a3b24e6b2737
joehall87 <joehall87@gmail.com>
2023-11-30 20:34:31 +0000

    Practise run on day 1 2022

--- 2023/src/day1.rs
+++ 2023/src/day1.rs
@@ -1,7 +1,20 @@
 pub fn run(content: String) {
     let parts = content.split("\n").map(|line| line.trim());
+    let mut buffer = 0;
+    let mut biggest = 0;
     for part in parts {
-        dbg!(part.len());
-        println!("{}", part);
+        if part.len() == 0 {
+            if buffer > biggest {
+                biggest = buffer
+            };
+            buffer = 0;
+            continue;
+        }
+        let value: u32 = part.parse().expect("Noo");
+        buffer += value;
     }
+    if buffer > biggest {
+        biggest = buffer
+    };
+    println!("PART 1: {}", biggest);
 }
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -6,7 +6,7 @@ mod day1;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let data = Data::new(&args);
+    let data: Data = Data::new(&args);
     
     if data.day == "1" {
         day1::run(data.content);
@@ -20,9 +20,9 @@ struct Data {
 
 impl Data {
     fn new(args: &[String]) -> Data {
-        let day = &args[1].trim();
+        let day = args[1].trim();
         let path = format!(
-            "C:\\Users\\7082tr\\code\\adventofcode\\2023\\data\\day{day}.txt");
+            "/Users/Joe/src/adventofcode/2023/data/day{day}.txt");
 
         let error_msg = format!("Cannot find {path}");
         let content = fs::read_to_string(path).expect(&error_msg);
