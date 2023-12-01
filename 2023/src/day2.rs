cb5eaaaef6d14397d6e4ffbba11478ff325960f9
Joseph Hall <joseph.hall@bp.com>
2023-12-01 10:32:33 +0000

    Add placeholders for first 8 days

---
+++ 2023/src/day2.rs
@@ -0,0 +1,14 @@
+pub fn run(content: String) {
+    part1(&content);
+    part2(&content);
+}
+
+
+fn part1(content: &String) {
+    println!("PART 1: {}", -1);
+}
+
+
+fn part2(content: &String) {
+    println!("PART 2: {}", -1);
+}
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -3,19 +3,40 @@ use std::fs;
 use std::path;
 
 mod day1;
+mod day2;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let data: Data = Data::new(&args);
     
-    if data.day.starts_with("1") {
+    if (data.suffix == "1") | (data.suffix.starts_with("1_")) {
         day1::run(data.content);
+    } else if (data.suffix == "2") | (data.suffix.starts_with("2_")) {
+        day2::run(data.content);
+    } else if (data.suffix == "3") | (data.suffix.starts_with("3_")) {
+        day3::run(data.content);
+    } else if (data.suffix == "4") | (data.suffix.starts_with("4_")) {
+        day4::run(data.content);
+    } else if (data.suffix == "5") | (data.suffix.starts_with("5_")) {
+        day5::run(data.content);
+    } else if (data.suffix == "6") | (data.suffix.starts_with("6_")) {
+        day6::run(data.content);
+    } else if (data.suffix == "7") | (data.suffix.starts_with("7_")) {
+        day7::run(data.content);
+    } else if (data.suffix == "8") | (data.suffix.starts_with("8_")) {
+        day8::run(data.content);
     }
 }
 
 struct Data {
-    day: String,
+    suffix: String,
     content: String,
 }
 
@@ -30,12 +51,12 @@ impl Data {
             .expect("Oops");
         let data_dir = root_dir.join(path::Path::new("data"));
 
-        let day = args[1].trim();
-        let filename = format!("day{day}.txt");
+        let suffix = args[1].trim();
+        let filename = format!("day{suffix}.txt");
         let filepath = data_dir.join(path::Path::new(&filename));
 
         let error_msg = format!("Can't find file!");
         let content = fs::read_to_string(filepath).expect(&error_msg);
-        Data { day: day.to_string(), content }
+        Data { suffix: suffix.to_string(), content }
     }
 }
