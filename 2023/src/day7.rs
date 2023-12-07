bb15dc0e46c95731ca819dc3f1846a107aabbd89
joehall87 <joehall87@gmail.com>
2023-12-07 14:02:02 +0000

    Day 7

--- 2023/src/day7.rs
+++ 2023/src/day7.rs
@@ -1,3 +1,5 @@
+// 50 mins for part 1, 1 hour for part 2
+
 use std::collections::HashMap;
 
 pub fn run(content: String) {
@@ -8,9 +10,7 @@ pub fn run(content: String) {
 
 fn part1(content: &str) {
     let mut hands = to_hands(content);
-    hands.sort_by_key(|x| x.0.sort_key());
-    let egg: Vec<&String> = hands.iter().map(|v| &v.0.cards).collect();
-    dbg!(egg);
+    hands.sort_by_cached_key(|x| x.0.sort_key());
     let winnings: usize = hands.iter()
         .enumerate()
         .map(|x| (x.0 + 1) * x.1.1)
@@ -19,8 +19,14 @@ fn part1(content: &str) {
 }
 
 
-fn part2(_content: &str) {
-    println!("PART 2: {}", -1);
+fn part2(content: &str) {
+    let mut hands = to_hands(content);
+    hands.sort_by_cached_key(|x| x.0.joker_sort_key());
+    let winnings: usize = hands.iter()
+        .enumerate()
+        .map(|x| (x.0 + 1) * x.1.1)
+        .sum();
+    println!("PART 2: {}", winnings);
 }
 
 
@@ -35,45 +41,89 @@ impl Hand {
     }
 
     fn sort_key(&self) -> (u32, u32, u32, u32, u32, u32) {
-        let mut counts: HashMap<char, u32> = HashMap::new();
+        let counts = self.to_counts();
+        let score = self.to_score(&counts);
+        self.to_key(score, 11)
+    }
+
+    fn joker_sort_key(&self) -> (u32, u32, u32, u32, u32, u32) {
+        let mut counts = self.to_counts();
+        let score = self.to_joker_score(&mut counts);
+        self.to_key(score, 1)
+    }
+
+    fn to_counts(&self) -> HashMap<char, u32> {
+        let mut counts = HashMap::new();
         for card in self.cards.chars() {
             counts.entry(card)
                 .and_modify(|v| *v += 1)
                 .or_insert(1);
         }
+        counts
+    }
+
+    fn to_joker_score(&self, counts: &mut HashMap<char, u32>) -> u32 {
+        if !counts.contains_key(&'J') {
+            return self.to_score(counts);  // no jokers
+        } else if counts.len() == 1 {
+            return 6;  // all jokers
+        }
+        let nj = counts.remove(&'J').unwrap();
+        let mut vals: Vec<&u32> = counts.values().collect();
+        vals.sort();
+        vals.reverse();
+
+        if *vals[0] + nj == 5 {
+            return 6;  // can make 5 of a kind
+        } else if *vals[0] + nj == 4 {
+            return 5;  // can make 4 of a kind
+        } else if (nj == 1) & (vals == vec![&2, &2]) {
+            return 4;  // can make full house
+        } else if (nj == 1) & (vals == vec![&2, &1, &1]) {
+            return 3;  // can make 3 of a kind
+        } else if (nj == 2) & (vals == vec![&1, &1, &1]) {
+            return 3;  // can make 3 of a kind
+        } else {
+            return 1;  // can always make a pair
+        }
+    }
+
+    fn to_score(&self, counts: &HashMap<char, u32>) -> u32 {
         let mut counts: Vec<&u32> = counts.values().collect();
         counts.sort();
         counts.reverse();
 
-        let mut kind = 0;
         if *counts[0] == 5 {
-            kind = 6  // 5 of a kind
+            return 6;  // 5 of a kind
         } else if *counts[0] == 4 {
-            kind = 5   // 4 of a kind
+            return 5;  // 4 of a kind
         } else if (*counts[0] == 3) & (*counts[1] == 2) {
-            kind = 4  // full house
+            return 4;  // full house
         } else if *counts[0] == 3 {
-            kind = 3  // 3 of a kind
+            return 3;  // 3 of a kind
         } else if (*counts[0] == 2) & (*counts[1] == 2) {
-            kind = 2  // 2 pairs
+            return 2;  // 2 pairs
         } else if *counts[0] == 2 {
-            kind = 1  // 1 pair
+            return 1;  // 1 pair
+        } else {
+            return 0;
         }
+    }
 
-        let values: Vec<u32> = 
+    fn to_key(&self, score: u32, joker: u32) -> (u32, u32, u32, u32, u32, u32) {
+        let v: Vec<u32> =
             self.cards.chars().map(
                 |c| match c {
                     'A' => 14,
                     'K' => 13,
                     'Q' => 12,
-                    'J' => 11,
+                    'J' => joker,
                     'T' => 10,
-                    _ => c.try_into().unwrap(),
+                    _ => c.to_digit(10).unwrap(),
                 }
             )
             .collect();
-
-        (kind, values[0], values[1], values[2], values[3], values[4])
+        (score, v[0], v[1], v[2], v[3], v[4])
     }
 }
 
