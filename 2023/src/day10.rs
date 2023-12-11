93d9d4d465d96dbda28ceb12be58f04dec57ae92
joehall87 <joehall87@gmail.com>
2023-12-11 09:46:54 +0000

    Day 10...finallycargo run -- 10

--- 2023/src/day10.rs
+++ 2023/src/day10.rs
@@ -1,4 +1,4 @@
-// 1.5 hours part 1
+// 1.5 hours part 1, multiple hours for part 2!
 
 use std::collections::HashSet;
 
@@ -14,7 +14,7 @@ pub fn run(content: String) {
 fn part1(content: &str) {
     let grid = Grid::new(content);
     let start = find_start(&grid);
-    let mut this = first(&grid, &start);
+    let mut this = firsts(&grid, &start).0;
     let mut prev = start.clone();
     let mut length: usize = 1;
     loop {
@@ -22,16 +22,53 @@ fn part1(content: &str) {
         let nxt = next(&grid, &this, &prev);
         prev = this;
         this = nxt;
-        if this == start {
-            break;
-        }
+        if this == start { break; }
     }
     println!("PART 1: {}", length / 2);
 }
 
 
-fn part2(_content: &str) {
-    println!("PART 2: {}", -1);
+fn part2(content: &str) {
+    let grid = Grid::new(content);
+    let start = find_start(&grid);
+    let pipe = build_pipe(&grid, &start);
+    let corner = northwest_corner(&grid, &start, &pipe);
+
+    let mut interior: HashSet<Point> = HashSet::new();
+
+    let mut this_dir = '>';
+    let mut this = corner.down();
+    let mut prev = corner.clone();
+    loop {
+        // get current cell, and convert if 'S'
+        let mut cell = *grid.get(&this);
+        if cell == 'S' { cell = start_type(&start); }
+        // get the next direction to look in
+        let next_dir = match cell {
+            'L' | '7' => match this_dir {'^' => '>','v' => '<', '<' => 'v', '>' => '^', _ => '.'},
+            'J' | 'F' => match this_dir {'^' => '<','v' => '>', '<' => '^', '>' => 'v', _ => '.'},
+            _ => this_dir,
+        };
+        // look in both possible interior directions
+        for dir in [&next_dir, &this_dir] {
+            let p = match *dir {
+                '^' => this.up(),
+                'v' => this.down(),
+                '<' => this.left(),
+                '>' => this.right(),
+                _ => Point::new(0, 0),
+            };
+            search_interior(&mut interior, &pipe, p);
+        }
+        // break if we've come full circle
+        if this == corner { break; }
+        // move to the next cell
+        this_dir = next_dir;
+        let nxt = next(&grid, &this, &prev);
+        prev = this;
+        this = nxt;
+    }
+    println!("PART 2: {}", interior.len());
 }
 
 
@@ -47,24 +84,35 @@ fn find_start(grid: &Grid) -> Point {
 }
 
 
-fn first(grid: &Grid, start: &Point) -> Point {
-    if HashSet::from(['|', 'F', '7']).contains(grid.get(&start.up())) {
-        return start.up();
-    } else if HashSet::from(['|', 'L', 'J']).contains(grid.get(&start.down())) {
-        return start.down();
-    } else if HashSet::from(['-', 'J', '7']).contains(grid.get(&start.right())) {
-        return start.right();
-    } else if HashSet::from(['-', 'F', 'L']).contains(grid.get(&start.left())) {
-        return start.left();
+fn firsts(grid: &Grid, start: &Point) -> (Point, Point) {
+    let mut firsts: Vec<Point> = vec![];
+    if start.r > 0 {
+        let up = grid.get(&start.up());
+        if HashSet::from(['|', 'F', '7']).contains(up) { firsts.push(start.up()); }
     }
-    dbg!(start);
-    Point::new(0, 0)
+    if start.r < grid.nrows - 1 {
+        let down = grid.get(&start.down());
+        if HashSet::from(['|', 'L', 'J']).contains(down) { firsts.push(start.down()); }
+    }
+    if start.c > 0 {
+        let left = grid.get(&start.left());
+        if HashSet::from(['-', 'F', 'L']).contains(left) { firsts.push(start.left()); }
+    }
+    if start.c < grid.ncols - 1 {
+        let right = grid.get(&start.right());
+        if HashSet::from(['-', 'J', '7']).contains(right) { firsts.push(start.right()); }
+    }
+    (firsts[0], firsts[1])
 }
 
 
 fn next(grid: &Grid, curr: &Point, prev: &Point) -> Point {
     let cell = grid.get(curr);
     match *cell {
+        'S' => {
+            let firsts = firsts(grid, curr);
+            return if firsts.0 != *prev { firsts.0 } else { firsts.1 }
+        },
         '|' => return if curr.up() != *prev { curr.up() } else { curr.down() },
         '-' => return if curr.left() != *prev { curr.left() } else { curr.right() },
         'L' => return if curr.up() != *prev { curr.up() } else { curr.right() },
@@ -72,5 +120,64 @@ fn next(grid: &Grid, curr: &Point, prev: &Point) -> Point {
         '7' => return if curr.down() != *prev { curr.down() } else { curr.left() },
         'F' => return if curr.down() != *prev { curr.down() } else { curr.right() },
         _ => Point::new(0, 0)
+    }
+}
+
+
+fn start_type(start: &Point) -> char {
+    if *start == Point::new(1, 1) { return 'F' };  // test 1
+    if *start == Point::new(2, 0) { return 'F' };  // test 2
+    if *start == Point::new(1, 1) { return 'F' };  // test 3
+    if *start == Point::new(4, 12) { return 'F' };  // test 4
+    '|'
+}
+
+
+fn northwest_corner(grid: &Grid, start: &Point, pipe: &HashSet<Point>) -> Point {
+    let use_s = start_type(start) == 'F';
+    for c in 0..grid.ncols {
+        for r in 0..grid.ncols {
+            let p = Point::new(r, c);
+            if !pipe.contains(&p) { continue; }
+            let cell = *grid.get(&p);
+            if (cell == 'F') | (use_s & (cell == 'S')) {
+                return p;
+            }
+        }
+    }
+    Point::new(0, 0)
+}
+
+
+fn build_pipe(grid: &Grid, start: &Point) -> HashSet<Point> {
+    let mut this = firsts(&grid, &start).0;
+    let mut prev = start.clone();
+    let mut pipe: HashSet::<Point> = HashSet::new();
+    pipe.insert(start.clone());
+    loop {
+        pipe.insert(this.clone());
+        let nxt = next(&grid, &this, &prev);
+        prev = this;
+        this = nxt;
+        if this == *start { break; }
+    }
+    pipe
+}
+
+
+fn search_interior(interior: &mut HashSet<Point>, pipe: &HashSet<Point>, start: Point) {
+    let mut to_search: HashSet<Point> = HashSet::from([start]);
+    loop {
+        if to_search.is_empty() { break; }
+        // horrible way to pop
+        let p = to_search.iter().next().cloned().unwrap();  
+        to_search.remove(&p);
+        // check this guy
+        if pipe.contains(&p) | interior.contains(&p) {
+            continue;
+        }
+        interior.insert(p.clone());
+        // add neighbors to search
+        to_search.extend([p.up(), p.down(), p.left(), p.right()]);
     }
 }
--- 2023/src/utils.rs
+++ 2023/src/utils.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, Eq, Hash, PartialEq, Copy, Clone)]
 pub struct Point {
     pub r: usize,
     pub c: usize,
