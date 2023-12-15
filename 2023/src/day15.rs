590b999ac03a47814a965a9ae7ec14aaac35f71b
joehall87 <joehall87@gmail.com>
2023-12-15 08:32:07 +0000

    Day 15

--- 2023/src/day15.rs
+++ 2023/src/day15.rs
@@ -1,14 +1,90 @@
+// 45 mins all in
+
+use itertools::Itertools;
+
 pub fn run(content: String) {
     part1(&content);
     part2(&content);
 }
 
 
-fn part1(_content: &str) {
-    println!("PART 1: {}", -1);
+fn part1(content: &str) {
+    let solution: usize = content.split(",").map(to_hash).sum();
+    println!("PART 1: {}", solution);
 }
 
 
-fn part2(_content: &str) {
-    println!("PART 2: {}", -1);
+fn part2(content: &str) {
+    let boxes = install(content);
+    let focusing_power: usize =
+        boxes.iter()
+        .enumerate()
+        .map(
+            |x| x.1.iter()
+                .enumerate()
+                .map(|y|
+                    (x.0 + 1) * (y.0 + 1) * y.1.focal_length
+                )
+                .sum::<usize>()
+        )
+        .sum();
+    println!("PART 2: {}", focusing_power);
+}
+
+
+#[derive(Debug, Clone)]
+struct Lens {
+    label: String,
+    focal_length: usize,
+}
+
+impl Lens {
+    fn new(label: &str, focal_length: usize) -> Lens {
+        Lens{ label: String::from(label), focal_length }
+    }
+}
+
+
+fn install(content: &str) -> Vec<Vec<Lens>> {
+    let mut boxes: Vec<Vec<Lens>> = vec![vec![]; 256];
+    for step in content.split(",") {
+        // update or add lens
+        if step.contains("=") {
+            let parts = step.split("=").collect_vec();
+            let box_num = to_hash(parts[0]);
+            let focal_length: usize = parts[1].parse().unwrap();
+            let lens = Lens::new(parts[0], focal_length);
+            let mut found = false;
+            for (i, existing) in boxes[box_num].iter().enumerate() {
+                if existing.label == lens.label {
+                    found = true;
+                    boxes[box_num][i] = lens.clone();
+                    break;
+                }
+            }
+            if !found {
+                boxes[box_num].push(lens);
+            }
+        // or remove lens
+        } else {
+            let key = String::from(&step[..step.len() - 1]);
+            let box_num = to_hash(&key);
+            let list = boxes[box_num].clone();
+            for (i, existing) in list.iter().enumerate() {
+                if existing.label == key {
+                    boxes[box_num].remove(i);
+                }
+            }
+        }
+    }
+    boxes
+}
+
+
+fn to_hash(step: &str) -> usize {
+    let mut value = 0usize;
+    for c in step.chars() {
+        value = ((value + c as usize) * 17) % 256;
+    }
+    value
 }
